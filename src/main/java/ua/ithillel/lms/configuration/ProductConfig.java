497f8882167fdde47cc608254a1e2fda139bd2a4
Pavlo Pashchevskyi <googalltooth4@gmail.com>
2023-01-21 13:52:47 +0200

    ProductList is now injecting by 1 configuration method, but not each configuration method separately

--- src/main/java/ua/ithillel/lms/configuration/ProductConfig.java
+++ src/main/java/ua/ithillel/lms/configuration/ProductConfig.java
@@ -11,56 +11,16 @@ import ua.ithillel.lms.model.ProductList;
 public class ProductConfig {
 
   @Bean
-  public Product product1() {
-    return new Product(1, "Asus", 59999.99);
-  }
-
-  @Bean
-  public Product product2() {
-    return new Product(2, "Acer", 49999.99);
-  }
-
-  @Bean
-  public Product product3() {
-    return new Product(3, "Asus Battery", 1700);
-  }
-
-  @Bean
-  public Product product4() {
-    return new Product(4, "Acer Battery", 1500);
-  }
-
-  @Bean
-  public Product product5() {
-    return new Product(5, "Luxeon UPS", 6399.99);
-  }
-
-  @Bean
-  public Product product6() {
-    return new Product(6, "Luxeon Voltage Corrector", 6399.99);
-  }
-
-  @Bean
-  public Product product7() {
-    return new Product(7, "Samsung air condition", 50590.51);
-  }
-
-  @Bean
-  public Product product8() {
-    return new Product(8, "Samsung refrigerator", 63999.99);
-  }
-
-  @Bean
   public ProductList productList() {
     List<Product> pl = new ArrayList<>();
-    pl.add(product1());
-    pl.add(product2());
-    pl.add(product3());
-    pl.add(product4());
-    pl.add(product5());
-    pl.add(product6());
-    pl.add(product7());
-    pl.add(product8());
+    pl.add(new Product(1, "Asus", 59999.99));
+    pl.add(new Product(2, "Acer", 49999.99));
+    pl.add(new Product(3, "Asus Battery", 1700));
+    pl.add(new Product(4, "Acer Battery", 1500));
+    pl.add(new Product(5, "Luxeon UPS", 6399.99));
+    pl.add(new Product(6, "Luxeon Voltage Corrector", 6399.99));
+    pl.add(new Product(7, "Samsung air condition", 50590.51));
+    pl.add(new Product(8, "Samsung refrigerator", 63999.99));
     return new ProductList(pl);
   }
 }
